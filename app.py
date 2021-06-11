5fc200172f32516d629e93538ce912758750117f
jamesr1775 <james.rooney@ucdconnect.ie>
2021-06-11 21:28:52 +0000

    adding register template and also connecting to the mongo database to update user registered. requirements update to include pymongo and dnspython

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import os
-from flask import Flask, render_template
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 @app.route("/")
 
@@ -13,6 +23,26 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        user_exists = mongo.db.users.find_one({"username": request.form.get("username").lower()})
+        password_match =  request.form.get("password").lower() == request.form.get("pwconfirm").lower()
+        if user_exists:
+            flash("Invalid Username: That username already exists!")
+            return redirect(url_for("register"))
+        if not password_match:
+            flash("Password Error: Confirmation Password does not match.")
+            return redirect(url_for("register"))
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password")),
+            "email": request.form.get("email")
+        }
+        mongo.db.users.insert_one(register)
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+    return render_template("register.html")
 
 
 if __name__ == "__main__":

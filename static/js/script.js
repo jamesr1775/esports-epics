aef40469080570d6c6347bb6f3b4d9a08a2a9e19
jamesr1775 <james.rooney@ucdconnect.ie>
2021-06-14 16:18:27 +0000

    update comment on script

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.sidenav').sidenav( {edge: "right"});
 });
 
-// Obtained from [1] in readme acknowledgements.
+// Verify Password Code, Obtained from [1] in readme acknowledgements.
 $("#pwconfirm").on("keyup", function (e) {
     if ($("#password").val() != $(this).val()) {
         $(this).removeClass("valid").addClass("invalid");

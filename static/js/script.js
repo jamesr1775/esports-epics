31b58f99995230036f1e3f1bff30433bfb5b0898
jamesr1775 <james.rooney@ucdconnect.ie>
2021-06-25 12:08:08 +0000

    changing the video modal to materialize instead of bootstrap as having bootstrap aswell as materialize distorted my forms greatly

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.sidenav').sidenav( {edge: "right"});
-    autoPlayYouTubeModal();
 });
+
 
 // Verify Password Code, Obtained from [1] in readme acknowledgements.
 $("#pwconfirm").on("keyup", function (e) {
@@ -13,16 +13,14 @@ $("#pwconfirm").on("keyup", function (e) {
 });
 
 
-//FUNCTION TO GET AND AUTO PLAY YOUTUBE VIDEO FROM DATATAG
-function autoPlayYouTubeModal(){
-  var trigger = $("body").find('[data-toggle="modal"]');
-  trigger.click(function() {
+// function that will open the youtube video in the modal and autoplay it. The setting src of the iframe was used from [3] in readme file.
+$('.modal-trigger').on("click", function() { 
     var theModal = $(this).data( "target" ),
     videoSRC = $(this).attr( "data-theVideo" ), 
     videoSRCauto = videoSRC+"?autoplay=1" ;
-    $(theModal+' iframe').attr('src', videoSRCauto);
-    $(theModal+' button.close').click(function () {
-        $(theModal+' iframe').attr('src', videoSRC);
-    });   
-  });
-}
+    $('#' +theModal+' iframe').attr('src', videoSRCauto);
+    $('#' +theModal+' .modal-close').click(function () {
+        $('#' + theModal+' iframe').attr('src', videoSRC);
+    });
+    $('#' + theModal).modal();
+})
